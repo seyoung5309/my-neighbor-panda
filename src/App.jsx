e2f4a258bc7536f7649d15045ef68b9448741100
오세영 <leenight5309@naver.com>
2026-07-19 04:20:22 +0900

    chore: 라우터 추가 하기

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import AddIngredientsPage from "./pages/AddIngredientsPage.jsx";
 import RegisterStep1 from "./pages/register/RegisterStep1.jsx";
 import RegisterStep2 from "./pages/register/RegisterStep2.jsx";
 import RegisterStep3 from "./pages/register/RegisterStep3.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import ChatRoomPageWrapper from "./pages/ChatRoomPageWrapper.jsx";
+import Navigation from "./components/Navigation.jsx";
 
 function App() {
   return (
@@ -16,7 +19,10 @@ function App() {
         <Route path="/register/step1" element={<RegisterStep1 />} />
         <Route path="/register/step2" element={<RegisterStep2 />} />
         <Route path="/register/step3" element={<RegisterStep3 />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/chat/:chatroomId" element={<ChatRoomPageWrapper />} />
       </Routes>
+      <Navigation />
     </BrowserRouter>
   );
 }
