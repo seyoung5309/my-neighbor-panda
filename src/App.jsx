af34f807fe20129e6192ac0ed3bcb48418ff71ee
오세영 <leenight5309@naver.com>
2026-07-19 04:16:50 +0900

    #chore: main이랑 진도 맞추기

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ChatPage from "./pages/ChatPage.jsx";
-import ChatRoomPageWrapper from "./pages/ChatRoomPageWrapper.jsx";
-import Navigation from "./components/navigation.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import AuthCallback from "./pages/AuthCallback.jsx";
+import AddIngredientsPage from "./pages/AddIngredientsPage.jsx";
+import RegisterStep1 from "./pages/register/RegisterStep1.jsx";
+import RegisterStep2 from "./pages/register/RegisterStep2.jsx";
+import RegisterStep3 from "./pages/register/RegisterStep3.jsx";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<ChatPage />} />
-                <Route path="/chat" element={<ChatPage />} />
-                <Route path="/chat/:chatroomId" element={<ChatRoomPageWrapper />} />
-            </Routes>
-            <Navigation />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
+        <Route path="/myingredients" element={<AddIngredientsPage />}></Route>
+        <Route path="/register/step1" element={<RegisterStep1 />} />
+        <Route path="/register/step2" element={<RegisterStep2 />} />
+        <Route path="/register/step3" element={<RegisterStep3 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
